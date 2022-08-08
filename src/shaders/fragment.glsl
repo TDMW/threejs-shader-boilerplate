7c8b03897f656db6732cfb8d88d89bae14431a94
Bart Simons <bart013@gmail.com>
2022-08-08 11:01:00 +0200

    Basic shader for starting

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,3 +1,9 @@
+#ifdef GL_OES_standard_derivatives
+#extension  GL_OES_standard_derivatives : enable
+#endif
+
+precision highp float;
+
 varying vec2 vUv;
 uniform vec3 u_color;
 uniform vec2 u_mouse;
@@ -5,11 +11,22 @@ uniform vec2 u_resolution;
 uniform float u_time;
 
 // uniform sampler2D uTexture;
-// uniform float uTime;
+float aastep(float threshold, float value) {
+  return step(threshold, value);
+}
 
+
+float time(float tt) {
+  return tt * 0.1;
+}
 
 void main() {
-  	vec3 color = vec3(u_mouse.x/u_resolution.x, 0.0, u_mouse.y/u_resolution.y);
-  
-  gl_FragColor = vec4(color, 1.0);
+  vec2 uv = (2. * gl_FragCoord.xy - u_resolution.xy) / u_resolution.y;
+
+  uv += 0.1 * sin(uv.yx * 10. + time(u_time) + sin(uv.xy * 7. + 2.1));
+
+  float g = aastep(.35, length(fract(uv * 10.) - .5));
+  vec3 color = vec3(g,g,g);
+
+  gl_FragColor = vec4(pow(color, vec3(1. / 1.2)), 1.0);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,10 +1,11 @@
+precision highp float;
+
 varying vec2 vUv;
 uniform vec2 uAspectRatio;
-
 
 void main() {
   // vUv = (uv - vec2(0.5)) * uAspectRatio + vec2(0.5);
   vUv = uv;
 
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position.xyz, 1.0);
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
